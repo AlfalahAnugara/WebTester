fc24f68004a6f59a78f582d87ba0a837d0ff6312
AlfalahAnugara <Alfalah190299@gmail.com>
2019-11-04 01:43:45 +0700

    All 5 test case done

--- WebTester/src/Kelompok14/RegisterEmailAddress.java
+++ WebTester/src/Kelompok14/RegisterEmailAddress.java
@@ -1,5 +1,6 @@
 package Kelompok14;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,7 +11,19 @@ public class RegisterEmailAddress {
 		WebDriver driver = new ChromeDriver() ;
 		driver.get("https://store.steampowered.com/join") ;
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("alfalah190299@gmail.com");
+		String at = driver.getTitle() ;
+		String et = "steam" ;
+		driver.close() ;
+		if(at.equalsIgnoreCase(et)) {
+			System.out.println("Test Berhasil") ;
+		} else {
+			System.out.println("Test Gagal") ;
+		}
 
 	}
 
-}
+	}
+
+
