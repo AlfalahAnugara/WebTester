81ee115cf3aeee934d7fa8afe70bc2a60f4b8c2b
AlfalahAnugara <Alfalah190299@gmail.com>
2019-11-04 01:32:25 +0700

    Test Input Password Done

--- WebTester/src/Kelompok14/InputPassword.java
+++ WebTester/src/Kelompok14/InputPassword.java
@@ -1,9 +1,31 @@
 package Kelompok14;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class InputPassword {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver() ;
+		driver.get("https://store.steampowered.com/login/") ;
+		driver.manage().window().maximize();
+		
+		WebElement password=driver.findElement(By.name("password")) ;
+		password.sendKeys("password") ;
+		String at = driver.getTitle() ;
+		String et = "gmail" ;
+		driver.close() ;
+		if(at.equalsIgnoreCase(et)) {
+			System.out.println("Test Berhasil") ;
+		} else {
+			System.out.println("Test Gagal") ;
+		}
+		
 		
 	}
+	
 
 }
