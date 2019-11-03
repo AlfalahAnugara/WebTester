628dc1f6be139baa4ec7ce3eefcc58ef7b300472
AlfalahAnugara <Alfalah190299@gmail.com>
2019-11-03 23:23:43 +0700

    HomeSteam done

--- WebTester/src/Kelompok14/HomeSteam.java
+++ WebTester/src/Kelompok14/HomeSteam.java
@@ -1,5 +1,26 @@
 package Kelompok14;
 
-public class HomeSteam {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class HomeSteam {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver() ;
+		driver.get("https://store.steampowered.com") ;
+		driver.manage().window().maximize();
+		driver.findElement(By.id("store_nav_search_term")).sendKeys("Tom Clancy");
+		driver.findElement(By.className("searchbox")).click() ;
+		String at = driver.getTitle() ;
+		String et = "steam" ;
+		driver.close() ;
+		if(at.equalsIgnoreCase(et)) {
+			System.out.println("Test Berhasil") ;
+		} else {
+			System.out.println("Test Gagal") ;
+		}
+
+	}
 }
+
