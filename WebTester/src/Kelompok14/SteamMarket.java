bff6ca3027a8c7ad55b5846bdd4b7ea16cb2259c
AlfalahAnugara <Alfalah190299@gmail.com>
2019-11-04 01:05:05 +0700

    Steam Market Done

--- WebTester/src/Kelompok14/SteamMarket.java
+++ WebTester/src/Kelompok14/SteamMarket.java
@@ -4,19 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Gmail {
+public class SteamMarket {
 
 	public static void main(String[] args) {
-		
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver() ;
-		driver.get("http://www.gmail.com/") ;
+		driver.get("https://steamcommunity.com/market/search?q=") ;
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.id("identifierId")).sendKeys("alfalah190299@gmail.com");
-		driver.findElement(By.className("CwaK9")).click() ;
+		driver.findElement(By.id("findItemsSearchBox")).sendKeys("CS20 Case");
+		driver.findElement(By.className("market_search_submit_button")).click() ;
 		String at = driver.getTitle() ;
-		String et = "gmail" ;
+		String et = "steam" ;
 		driver.close() ;
 		if(at.equalsIgnoreCase(et)) {
 			System.out.println("Test Berhasil") ;
@@ -25,5 +24,8 @@ public class Gmail {
 		}
 
 	}
+		
 
-}
+	}
+
+
