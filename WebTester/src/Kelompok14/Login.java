98e822bbc65147892f6aa1731425d2c62eddea08
AlfalahAnugara <Alfalah190299@gmail.com>
2019-11-05 13:36:57 +0700

    Rename Class

--- WebTester/src/Kelompok14/Login.java
+++ WebTester/src/Kelompok14/Login.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class InputPassword {
+public class Login {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
